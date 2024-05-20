fb23789b9077ba3709f63f3aaedc41b85c998d10
gopal <gopal@student.21-school.ru>
2024-05-20 06:10:54 +0300

    Updates

--- src/setupTests.ts
+++
@@ -1,5 +0,0 @@
-import '@testing-library/jest-dom';
-// for jest
-import { TextEncoder, TextDecoder } from 'text-encoding';
-global.TextEncoder = TextEncoder;
-global.TextDecoder = TextDecoder;
--- src/utils/decodeHtmlEntities.ts
+++
@@ -1,5 +0,0 @@
-export const decodeHtmlEntities = (text: string): string => {
-  const textarea = document.createElement("textarea");
-  textarea.innerHTML = text;
-  return textarea.value;
-};

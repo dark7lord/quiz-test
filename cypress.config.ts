6f6d058e36fd89c1516f2cd8de21b3adaab1fe63
gopal <gopal@student.21-school.ru>
2024-05-20 04:41:48 +0300

    lint: coorect error linters

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  // e2e: {
-  //   setupNodeEvents(on, config) {
-  //     // implement node event listeners here
-  //   },
-  // },
+  e2e: {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
 
   component: {
     devServer: {

cd2b2d52f53b168cebef83dbd1ea2cb7dfe65138
gopal <gopal@student.21-school.ru>
2024-05-20 05:13:26 +0300

    lint: correct error linters (for CI-CD)

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    setupNodeEvents(on, config) {
+    setupNodeEvents() {
       // implement node event listeners here
     },
   },
